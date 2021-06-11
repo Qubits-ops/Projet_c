894a4358903c3abcdff183f750d007a75ad0c854
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-06-11 20:26:44 +0200

    Create my_isneg.c

---
+++ Projet1/my_isneg.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+  write(1,&c,1);
+}
+
+int my_isneg(int n)
+{
+  if(n < 0){
+    my_putchar('N');
+  }else{
+    my_putchar('P');
+  }
+
+}
+
+
+int main()
+{
+  my_isneg(-1);
+  my_putchar('\n');
+
+}
+
+
